639151d363a61f2068d0a9ce66fc817251cad50c
alexjilavu29 <alexandrujilavu29@gmail.com>
2024-08-16 17:57:23 +0300

    Add web_scraper.py File to Project Folder -- 7th Commit

--- web_scraper.py
+++ web_scraper.py
@@ -20,6 +20,7 @@ user_agents = [
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Firefox/89.0',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Firefox/89.0',
 ]
 
 session = requests.Session()
